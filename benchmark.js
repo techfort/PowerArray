a284cf75ee9a3ab5fafc3167bb9ec61f8f6a6ec0
Joe Minichino <joe.minichino@gmail.com>
2014-11-26 15:02:24 +0000

    re-implemented power array extending Array so not to replicate native methods

--- benchmark.js
+++ benchmark.js
@@ -1,13 +1,9 @@
-var power = require('./index.js'),
-  SuppedArray = power.SuppedArray,
-  SuperPowerArray = power.SuperPowerArray;
+var PowerArray = require('./index.js');
 
 var i = 0,
   LEN = 1e7,
   array = [],
-  supped = new SuppedArray(),
-  sup = new SuperPowerArray(new ArrayBuffer(1e7)),
-  pwr = power.PowerArray();
+  supped = new PowerArray();
 
 function rand() {
   return Math.floor(Math.random() * 100 + 1);
@@ -26,8 +22,6 @@ for (i; i < LEN; i += 1) {
   var rnd = rand();
   array.push(rnd);
   supped.push(rnd);
-  sup.push(rnd);
-  pwr.push(rnd);
   if (i === LEN / 10) {
     console.log('10% complete');
   }
@@ -56,9 +50,8 @@ for (i; i < LEN; i += 1) {
     console.log('90% complete');
   }
 }
-console.log('100% complete');
+console.log('Arrays populated');
 
-console.log('Array.forEach');
 var s1 = process.hrtime();
 array.forEach(function (i) {
   i * 2;
@@ -67,55 +60,31 @@ var plain = process.hrtime(s1);
 console.log('Array.forEach complete in ' + format(plain) + ', ops/s: ' + ops_per_sec(plain));
 
 i = LEN;
-console.log('plain for loop');
 var s3 = process.hrtime();
 while (i--) {
   array[i] * 2;
 }
-var s4 = process.hrtime(s3);
-console.log('For loop complete in ' + format(s4) + ', ops/s: ' + ops_per_sec(s4));
-
-console.log('SuperPowerArray');
-var s7 = process.hrtime();
-sup.forEach(function (i) {
-  i * 2;
-});
-var s8 = process.hrtime(s7);
-console.log('SuperPowerArray complete in ' + format(s8) + ', ops/s: ' + ops_per_sec(s8));
+var forloop = process.hrtime(s3);
+console.log('For loop complete in ' + format(forloop) + ', ops/s: ' + ops_per_sec(forloop));
 
 
-console.log('PowerArray');
-var s9 = process.hrtime();
-pwr.forEach(function (i) {
-  i * 2;
-});
-var power_bm = process.hrtime(s9);
-console.log('PowerArray complete in ' + format(power_bm) + ', ops/s: ' + ops_per_sec(power_bm));
-console.log('---------------------------------------------------------------------------------');
-console.log('Power/Plain forEach ratio: ' + ops_per_sec(power_bm) / ops_per_sec(plain));
-console.log('---------------------------------------------------------------------------------');
-console.log('Power/ for-loop forEach ratio: ' + ops_per_sec(power_bm) / ops_per_sec(s4));
-console.log('---------------------------------------------------------------------------------');
-
-console.log('SuppedArray');
 var s5 = process.hrtime();
 supped.forEach(function (i) {
   i * 2;
 });
-var s6 = process.hrtime(s5);
-console.log('SuppedArray complete in ' + format(s6) + ', ops/s: ' + ops_per_sec(s6));
+var supped_bm = process.hrtime(s5);
+console.log('SuppedArray complete in ' + format(supped_bm) + ', ops/s: ' + ops_per_sec(supped_bm));
 
+console.log('---------------------------------------------------------------------------------');
+console.log('Supped/ forEach ratio: ' + ops_per_sec(supped_bm) / ops_per_sec(plain));
+console.log('---------------------------------------------------------------------------------');
+console.log('Supped/ for-loop ratio: ' + ops_per_sec(supped_bm) / ops_per_sec(forloop));
+console.log('---------------------------------------------------------------------------------');
+
+console.log('\n\n');
 console.log('-------------');
 console.log('MAP benchmark');
 console.log('-------------')
-
-console.log('PowerArray MAP');
-var wamap = process.hrtime();
-pwr.map(function (i) {
-  return i * 2;
-});
-var wamapend = process.hrtime(wamap);
-console.log('PowerArray MAP complete in ' + format(wamapend) + ', ops/s: ' + ops_per_sec(wamapend));
 
 console.log('Plain Array MAP');
 var map = process.hrtime();
@@ -123,6 +92,16 @@ array.map(function (i) {
   return i * 2;
 });
 var mapend = process.hrtime(map);
+
+console.log('SuppedArray MAP');
+var supmap = process.hrtime();
+supped.map(function (i) {
+  return i * 2;
+});
+var supmapend = process.hrtime(supmap);
+
+console.log();
+
 console.log('Plain Array MAP complete in ' + format(mapend) + ', ops/s: ' + ops_per_sec(mapend));
 console.log('---------------------------------------------------------------------------------');
-console.log('Power/Plain Ratio: ' + (ops_per_sec(wamapend) / ops_per_sec(mapend)).toFixed(2));
+console.log('Supped/Plain Ratio: ' + (ops_per_sec(supmapend) / ops_per_sec(mapend)).toFixed(2));
--- index.js
+++ index.js
@@ -1,92 +1,43 @@
-function SuppedArray() {}
-
-SuppedArray.prototype = new Array;
-SuppedArray.prototype.forEach = function (fun) {
-  for (var len = this.length; len--;) {
-    fun(this[len]);
-  }
-};
-
-function SuperPowerArray(len) {
-  this.usedLength = 0;
-}
-
-SuperPowerArray.prototype = new Uint8Array;
-SuperPowerArray.prototype.forEach = function (fun) {
-  for (var i = this.length; i--;) {
-    fun(this[i]);
-  }
-};
-
-SuperPowerArray.prototype.push = function (val) {
-  this[this.usedLength] = val;
-  this.usedLength += 1;
-};
-
 function loop(fun) {
   var i = 0,
     len = this.length;
+
   for (i; i < len; i += 1) {
     fun(this[i], i);
   }
 }
 
-function PowerArray() {
-  var array = [];
-
-  return {
-    push: array.push,
-    forEach: loop.bind(array),
-    map: function (fun) {
-      var results = PowerArray();
-      array.forEach(function (elem) {
-        results.push(fun(elem));
-      });
-      return results;
-    },
-    at: function (i) {
-      return array[i];
-    },
-    // UNINMPLEMENTED METHODS
-
-    from: array.from,
-    isArray: array.isArray,
-    of: array.of,
-    concat: array.concat,
-    contains: array.contains,
-    copyWithin: array.copyWithin,
-    entries: array.entries,
-    every: array.every,
-    fill: array.fill,
-    filter: array.filter,
-    find: array.find,
-    findIndex: array.findIndex,
-    indexOf: array.indexOf,
-    join: array.join,
-    keys: array.keys,
-    lastIndexOf: array.lastIndexOf,
-    //map: array.map,
-    pop: array.pop,
-    push: array.push,
-    reduce: array.reduce,
-    reduceRight: array.reduceRight,
-    reverse: array.reverse,
-    shift: array.shift,
-    slice: array.slice,
-    some: array.some,
-    sort: array.sort,
-    splice: array.splice,
-    toLocaleString: array.toLocaleString,
-    toSource: array.toSource,
-    toString: array.toString,
-    unshift: array.unshift,
-    values: array.values
-
-  };
-}
-
-module.exports = {
-  SuppedArray: SuppedArray,
-  SuperPowerArray: SuperPowerArray,
-  PowerArray: PowerArray
+// PowerArray
+function PowerArray() {}
+PowerArray.prototype = new Array;
+PowerArray.prototype.forEach = loop.bind(this);
+PowerArray.prototype.map = function (fun) {
+  var results = new PowerArray();
+  this.forEach(function (elem) {
+    results.push(fun(elem));
+  });
+  return results;
 };
+PowerArray.prototype.filter = function (filterFun) {
+  var results = new PowerArray();
+  this.forEach(function (elem) {
+    if (filterFun(elem)) {
+      results.push(elem);
+    }
+  });
+  return results;
+};
+PowerArray.prototype.contains = function (elem) {
+  var i = 0,
+    len = this.length,
+    found = false;
+  for (i; i < len; i += 1) {
+    if (this[i] === elem) {
+      found = true;
+      break;
+    }
+  }
+  return found;
+};
+
+module.exports = PowerArray;
