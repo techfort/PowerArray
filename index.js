becd9ecd296b4558d9ffb3440343bc5eac52dc1a
Joe Minichino <joe.minichino@gmail.com>
2014-11-28 13:44:44 +0000

    fixed merge of pull requests, reappleid stashed changes

--- index.js
+++ index.js
@@ -1,30 +1,53 @@
+/*
+ * PowerArray
+ * 5x faster array performance with built-in binarySearch
+ * @author joe minichino <joe.minichino@gmail.com>
+ * License: BSD
+ */
+
 'use strict';
 
-// PowerArray
-function PowerArray(arr) {
-  if (arguments.length > 0) {
-    var self = this;
-    PowerArray.prototype.forEach.call(arr, function (elem) {
-      self.push(elem);
-    });
-  }
+
+/**
+ * PowerArray
+ * @constructor
+ * @param {array} optional plain array to populate a power array
+ */
+function PowerArray(array) {
+  var load = array || [],
+    self = this;
+  load.forEach(function (el) {
+    self.push(el);
+  });
 }
 
+/*
+ * inheritance
+ */
 PowerArray.prototype = new Array;
 
+/**
+ * addAndSort inserts a value and sorts the array after insertion to keep binary search working
+ * @param {number} val
+ */
 PowerArray.prototype.addhAndSort = function (val) {
   this.push(val);
   this.numericSort();
 };
 
+/**
+ * forEach: improved version of the Array.prototype.forEach method, utilises for loop
+ *
+ * @param {function} processing function
+ */
 PowerArray.prototype.forEach = function (fun) {
   var i = 0,
     len = this.length;
-
   for (; i < len; i++) {
     fun(this[i], i);
   }
 };
+
 
 PowerArray.prototype.concat = function (arr) {
   var results = new PowerArray(this);
@@ -34,22 +57,44 @@ PowerArray.prototype.concat = function (arr) {
   return results;
 };
 
+/**
+ * map: improved version of map
+ * re-utilizing PowerArray.forEach did not make performance differences, so for-loop was used to maintain the 5x factor
+ *
+ * @param {function} mapping function
+ * @returns {array} mapped array
+ */
 PowerArray.prototype.map = function (fun) {
-  var results = new PowerArray();
-  this.forEach(function (elem, index) {
-    results.push(fun(elem, index));
-  });
+  var results = new PowerArray(),
+    i;
+  for (i = 0; i < this.length; i += 1) {
+    results.push(fun(this[i], i));
+  }
   return results;
 };
+
+/**
+ * filter: improved version of map
+ *
+ * @param {function} filter function
+ * @returns {array} filtered array
+ */
 PowerArray.prototype.filter = function (filterFun) {
   var results = new PowerArray();
-  this.forEach(function (elem, index) {
-    if (filterFun(elem, index)) {
-      results.push(elem);
+  for (i = 0; i < this.length; i += 1) {
+    if (filterFun(this[i], i)) {
+      results.push(this[i]);
     }
-  });
+  }
   return results;
-};
+}
+
+/**
+ * contains: returns whether the array contains an element or not
+ *
+ * @param element
+ * @returns {boolean} is the element contained or not
+ */
 PowerArray.prototype.contains = function (elem) {
   var i = 0,
     len = this.length,
@@ -63,13 +108,17 @@ PowerArray.prototype.contains = function (elem) {
   return found;
 };
 
+/**
+ * numericSort: sorts the array if the array is numerical
+ *
+ */
 PowerArray.prototype.numericSort = function (fun) {
   this.sort(fun || function (a, b) {
     return a < b ? -1 : 1;
   });
 };
 
-// Binary Search method, only works if array is sorted.
+
 PowerArray.prototype.binarySearch = function (searchElement) {
 
   var minIndex = 0;
